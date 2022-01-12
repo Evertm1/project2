d333b646d2e622979c28e0c774cd06804e7c759a
Programming <programming@Marcos-MacBook-Pro.local>
2022-01-11 19:17:25 -0800

    added delete functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,7 +1,8 @@
 const Coffee = require('../models/coffee');
 
 module.exports = {
-	create 
+	create,
+    delete: deleteReview, 
 }
 
 
@@ -25,3 +26,19 @@ function create(req, res){
     })
 
 }
+
+ function deleteReview(req, res){
+     // query the particlular review on a given coffee object
+    
+        Coffee.findById(req.params.id, async function (err, coffeeDocument){
+        try {
+            console.log("delete function");
+        coffeeDocument.reviews.pull( {_id: req.params.reviewId})
+        await coffeeDocument.save()
+        res.redirect(`/coffees/description/${coffeeDocument._id}`)
+        } catch (error) {
+            console.log(error)
+        }
+        
+    })}
+
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,6 @@ const reviewsCtrl = require('../controllers/reviews');
 
 
 router.post('/coffees/description/:id', reviewsCtrl.create);
-
+router.delete('/coffees/description/:id/:reviewId', reviewsCtrl.delete);
 
 module.exports = router;
